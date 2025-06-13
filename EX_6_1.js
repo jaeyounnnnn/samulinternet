ded4165a30196db7f837d013a1aa83beeb2ca352
jaeyounnnnn <jaeyoun980623@naver.com>
2025-06-13 14:52:56 +0900

    프로토타입 체인 구조를 코드로 구현한 예시 코드

---
+++ EX_6_1.js
@@ -0,0 +1,9 @@
+var Person = function (name) {
+    this._name = name;
+  };
+  Person.prototype.getName = function () {
+    return this._name;
+  };
+  
+  var suzi = new Person('Suzi');
+  suzi.__proto__.getName();
